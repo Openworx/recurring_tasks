f5c8ec5f38212d1a4c484e7a3de20cce16ff0480
root <root@recurring-task-v9.lan>
2017-04-25 13:54:17 -0400

    Migrated to 9.0

---
+++ project_task_repetition/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Project task repetition",
+    'summary'       : 'Allows to set repetition of project tasks',
+    'description'   : """
+With this module you are able to repeat project tasks pretty much in the same way as you repeat calendar events.
+    """,
+
+    'author'        : 'Openworx',
+    'website'        : 'http://www.openworx.nl',
+    'license': 'AGPL-3',
+    'price': 39.00,
+    'currency': 'EUR',
+    'category': 'Project',
+    'version': '1.1',
+    'depends': ["project"],
+    'data': [
+        'project_task_repetition_view.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
